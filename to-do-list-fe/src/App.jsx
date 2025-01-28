e5c3f311e44c37412e37f6f520827c06dd5c51a3
Viv Hayles <vivhayles@gmail.com>
2025-01-28 16:29:26 +0000

    installed tailwind and resolved react router dom issue

--- to-do-list-fe/src/App.jsx
+++ to-do-list-fe/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ToDoListPage from "./Pages/ToDoListPage.jsx";
 
 function App() {
